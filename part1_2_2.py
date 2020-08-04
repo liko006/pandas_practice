111782685360a989065254fef77fc0e7dd716181
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-04 20:26:18 +0900

    Update part1_2_2.py

--- part1_2_2.py
+++ part1_2_2.py
@@ -13,6 +13,7 @@ print(type(df))
 print()
 # print the object which is saved in variable, 'df'
 print(df)
+print()
 
 # example part 1-5
 
@@ -25,6 +26,7 @@ print()
 print(df.index)   # index raw
 print()
 print(df.columns)   # column name
+print()
 
 # changing index of rows and names of columns
 df.index = ['student1', 'student2']
@@ -35,6 +37,7 @@ print()
 print(df.index)   # index raw
 print()
 print(df.column)   # column name
+print()
 
 # example part 1-6
 
@@ -53,11 +56,12 @@ df.rename(index={'Junseo':'student1', 'Yeeun':'student2'}, inplace=True)
 
 # print 'df' after change
 print(df)
+print()
 
 # example part 1-7
 
 # converting data type by using DataFrame() function and save as 'df'
-exam_data = {'math':[90,80,70], 'eng':[98,89,95], 'music':[85,95,100], 'phys_ed':[100,90,90]}
+exam_data = {'math':[90,80,70], 'eng':[98,89,95], 'music':[85,95,100], 'gym':[100,90,90]}
 
 df = pd.DataFrame(exam_data, index=['Seojoon', 'Woohyun', 'Ina'])
 print(df)
@@ -73,11 +77,12 @@ print()
 df3 = df[:]
 df3.drop(['Woohyun', 'Ina'], axis=0, inplace=True)
 print(df3)
+print()
 
 # example part 1-8
 
 # converting data type by using DataFrmae() function and save as 'df'
-exam_data = {'math':[90,80,70], 'eng':[98,89,95], 'music':[85,95,100], 'phys_ed':[100,90,90]}
+exam_data = {'math':[90,80,70], 'eng':[98,89,95], 'music':[85,95,100], 'gym':[100,90,90]}
 
 df = pd.DataFrame(exam_data, index=['Seojoon', 'Woohyun', 'Ina'])
 print(df)
@@ -97,7 +102,7 @@ print()
 
 # example part 1-9
 
-exam_data = {'math':[90,80,70], 'eng':[98,89,95], 'music':[85,95,100], 'phys_ed':[100,90,90]}
+exam_data = {'math':[90,80,70], 'eng':[98,89,95], 'music':[85,95,100], 'gym':[100,90,90]}
 
 df = pd.DataFrame(exam_data, index=['Seojoon', 'Woohyun', 'Ina'])
 print(df)
@@ -108,15 +113,80 @@ position1 = df.iloc[0]
 print(label1)
 print()
 print(position1)
+print()
 
 label2 = df.loc[['Seojoon', 'Woohyun']]
 position2 = df.iloc[[0,1]]
 print(label2)
 print()
 print(position2)
+print()
 
 label3 = df.loc[['Seojoon':'Woohyun']]
 position3 = df.iloc[[0:1]]
 print(label3)
 print()
 print(position3)
+print()
+
+# example part 1-10 
+
+exam_data = {'name': ['Seojoon', 'Woohyun', 'Ina'], 'math':[90,80,70], 'eng':[98,89,95], 'music':[85,95,100], 'gym':[100,90,90]}
+df = pd.DataFrame(exam_data)
+print(df)
+print(type(df))
+print()
+
+math1 = df['math']
+print(math1)
+print(type(math1))
+print()
+print()
+
+english = df.english
+print(english)
+print(type(english))
+
+music_gym = df[['music', 'gym']]
+print(music_gym)
+print(type(music_gym))
+print()
+
+math2 = df[['math']]
+print(math2)
+print(type(math2))
+print()
+
+# example part 1-11
+
+exam_data = {'name': ['Seojoon', 'Woohyun', 'Ina'], 'math':[90,80,70], 'eng':[98,89,95], 'music':[85,95,100], 'gym':[100,90,90]}
+df = pd.DataFrame(exam_data)
+
+df.set_index('name', inplace=True)
+print(df)
+print()
+
+a = df.loc['Seojoon', 'music']
+print(a)
+b = df.iloc[0, 2]
+print(b)
+print()
+
+c = df.loc['Seojoon', ['music', 'gym']]
+print(c)
+d = df.iloc[0, [2,3]]
+print(d)
+e = df.loc['Seojoon', 'music':'gym']
+print(e)
+f = df.iloc[0, 2:]
+print(f)
+print()
+
+g = df.loc[['Seojoon', 'Woohyun'], ['music', 'gym']]
+print(g)
+h = df.iloc[[0, 1], [2, 3]]
+print(h)
+i = df.loc['Seojoon':'Woohyun', 'music':'gym']
+print(i)
+j = df.iloc[0:2, 2:]
+print(j)
