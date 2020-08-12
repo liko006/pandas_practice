795879f71fb05c6738a4e52d55db3f43c718b051
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-12 17:21:34 +0900

    Create part1_3_1.py

---
+++ part1_3_1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+exam_data = {'name':['Seojoon','Woohyun','Ina'], 'math':[90,80,70], 'eng':[98,89,95], ' music':[85,95,100], 'gym':[100,90,90]}
+df = pd.DataFrame(exam_data)
+print(df)
+print()
+
+ndf = df.set_index(['name'])
+print(df)
+print()
+ndf2 = df.set_index(['music'])
+print(df)
+prin()
+ndf3 = df.sex_index(['math','music'])
+print(df)
+print()
