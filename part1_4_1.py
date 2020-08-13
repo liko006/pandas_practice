5e15573ee83ec8add72706c88cfb7b205bcdee62
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-13 11:37:47 +0900

    Update part1_4_1.py

--- part1_4_1.py
+++ part1_4_1.py
@@ -11,3 +11,25 @@ percentage = student1/200
 print(percentage)
 print()
 print(tpye(percentage))
+print()
+
+# example part 1-22
+
+student1 = pd.Series({'kor':100, 'eng':80, 'math':90})
+student2 = pd.Series({'math':80, 'kor':90, 'eng':80})
+print(student1)
+print()
+print(student2)
+print()
+
+addition = student1 + student2
+subtraction = student1 - student2
+multipliaction = student1 * student2
+division = student1 / student2
+print(type(division))
+print()
+
+result = pd.DataFrame([addition, subtraction, multiplication, division], index=['add','sub','mul','div'])
+print(result)
+print()
+
