cc428b849e2449f0b9d87990145a3891f92a2912
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-13 11:34:40 +0900

    Update part1_4_1.py

--- part1_4_1.py
+++ part1_4_1.py
@@ -2,3 +2,12 @@ import pandas as pd
 
 # example part 1-21
 
+student1 = pd.Series({'kor':100, 'eng':80, 'math':90})
+print(student1)
+print()
+
+percentage = student1/200
+
+print(percentage)
+print()
+print(tpye(percentage))
