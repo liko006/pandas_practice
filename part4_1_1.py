abceadf1f8550a9affcc0fba8f9d58e3c115ac4d
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-18 20:01:13 +0900

    Update part4_1_1.py

--- part4_1_1.py
+++ part4_1_1.py
@@ -43,3 +43,33 @@ plt.title('서울 -> 경기 인구 이동')
 plt.xlabel('기간')
 plt.ylabel('이동 인구수')
 plt.show()
+
+# example part 4-3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from matplotlib import font_manager, rc
+font_path = './malgun.ttf'
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font_name)
+
+df = pd.read_excel('./시도별 전출입 인구수.xlsx', fillna=0, header=0)
+
+df = df.fillna(method='ffill')
+
+temp = (df['전출지별'] == '서울특별시') & (df['전입지별'] != '서울특별시')
+
+df_seoul = df[temp]
+df_seoul = df_seoul.drop(['전출지별'], axis=1)
+df_seoul.rename({'전입지별':'전입지'}, axis=1, inplace=True)
+df_seoul.set_index('전입지', inplace=True)
+
+sr_one = df_seoul.loc['경기도']
+
+plt.plot(sr_one)
+
+plt.title('서울 -> 경기 인구 이동')
+plt.xlabel('기간')
+plt.ylabel('이동 인구수')
+plt.show()
