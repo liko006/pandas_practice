b9a72cb3fa02c545001115adaf41538961bc3593
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-20 20:54:57 +0900

    Update part4_1_1.py

--- part4_1_1.py
+++ part4_1_1.py
@@ -274,3 +274,39 @@ ax.tick_params(axis='x', labelsize=10)
 ax.tick_params(axis='y', labelsize=10)
 
 plt.show()
+
+# example part 4-11
+
+# same as part 4-10 until col_years = list(map(str, range(1970,2018)))
+
+df_4 = df_seoul.loc[['충청남도','경상북도','강원도','전라남도'], col_years]
+
+plt.style.use('ggplot')
+
+fig = plt.figure(figsize=(20,10))
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,2,3)
+ax4 = fig.add_subplot(2,2,4)
+
+ax1.plot(col_years, df_4.loc['충청남도',:], marker='o', markerfacecolor='green', markersize=10, color='olive', linewidth=2, label='서울 -> 충남')
+ax2.plot(col_years, df_4.loc['경상북도',:], marker='o', markerfacecolor='blue', markersize=10, color='skyblue', linewidth=2, label='서울 -> 경북')
+ax3.plot(col_years, df_4.loc['강원도',:], marker='o', markerfacecolor='red', markersize=10, color='magenta', linewidth=2, label='서울 -> 강원')
+ax4.plot(col_years, df_4.loc['전라남도',:], marker='o', markerfacecolor='orange', markersize=10, color='yellow', linewidth=2, label='서울 -> 전남')
+
+ax1.legend(loc='best')
+ax2.legend(loc='best')
+ax3.legend(loc='best')
+ax4.legend(loc='best')
+
+ax1.set_title('서울 -> 충북 인구 이동', size=15)
+ax2.set_title('서울 -> 경북 인구 이동', size=15)
+ax3.set_title('서울 -> 강원 인구 이동', size=15)
+ax4.set_title('서울 -> 전남 인구 이동', size=15)
+
+ax1.set_xticklabels(col_years, rotation=90)
+ax2.set_xticklabels(col_years, rotation=90)
+ax3.set_xticklabels(col_years, rotation=90)
+ax4.set_xticklabels(col_years, rotation=90)
+
+plt.show()
