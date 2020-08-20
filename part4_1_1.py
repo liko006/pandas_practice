87894f02bb516069d9af2615ae8f0c3ed9d097ec
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-20 20:31:29 +0900

    Update part4_1_1.py

--- part4_1_1.py
+++ part4_1_1.py
@@ -164,3 +164,44 @@ plt.annotate('인구 이동 감소(1995-2017)',    # contents of text
              fontsize=15)                   # size of text 
 
 plt.show()
+
+# example part 4-8
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from matplotlib import font_manager, rc
+font_path = './malgun.ttf'
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font_name)
+
+df = pd.read_excel('./시도별 전출입 인구수.xlsx', fillna=0, header=0)
+
+df = df.fillna(method='ffill')
+
+temp = (df['전출지별'] == '서울특별시') & (df['전입지별'] != '서울특별시')
+
+df_seoul = df[temp]
+df_seoul = df_seoul.drop(['전출지별'], axis=1)
+df_seoul.rename({'전입지별':'전입지'}, axis=1, inplace=True)
+df_seoul.set_index('전입지', inplace=True)
+
+sr_one = df_seoul.loc['경기도']
+
+plt.style.use('ggplot')
+
+fig = plt.figure(figsize=(10,10))
+ax1 = fig.add_subplot(2,1,1)
+ax2 = fig.add_subplot(2,1,2)
+
+ax1.plot(sr_one, 'o', markersize=10)
+ax2.plot(sr_one, marker='o', markerfacecolor='green', markersize=10, color='olive', linewidth=2, label='서울 -> 경기')
+ax2.legend(loc='best')
+
+ax1.set_ylim(50000, 800000)
+ax2.set_ylim(50000, 800000)
+
+ax1.set_xticklabels(sr_one.index, rotation=75)
+ax2.set_xticklabels(sr_one.index, rotation=75)
+
+plt.show()
