0c54e11cba9ca34386fb180e986e396f804593c3
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-22 11:06:11 +0900

    Update part4_1_3.py

--- part4_1_3.py
+++ part4_1_3.py
@@ -57,3 +57,41 @@ plt.ylabel('전입지')
 plt.xlabel('이동 인구 수')
 
 plt.show()
+
+# example part 4-18
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+font_path = './malgun.ttf'
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font_name)
+
+plt.style.use('ggplot')
+plt.rcParams['axes.unicode_minus']=False
+
+df = pd.read_excel('./참고자료/파이썬 머신러닝 판다스 데이터분석 자료/part4/남북한발전전력량.xlsx', convert_float=True)
+df = df.loc[5:9]
+df.drop('전력량 (억㎾h)', axis='columns', inplace=True)
+df.set_index('발전 전력별', inplace=True)
+df = df.T
+
+df = df.rename(columns={'합계':'총발전량'})
+df['총발전량 - 1년'] = df['총발전량'].shift(1)
+df['증감률'] = ((df['총발전량'] / df['총발전량 - 1년']) - 1) * 100
+
+ax1 = df[['수력','화력']].plot(kind='bar', figsize=(20,10), width=0.7, stacked=True)
+ax2 = ax1.twinx()
+ax2.plot(df.index, df.증감률, ls='--', marker='o', markersize=20, color='green', label='전년대비 증감률(%)')
+
+ax1.set_ylim(0,500)
+ax2.set_ylim(-50,50)
+
+ax1.set_xlabel('연도', size=20)
+ax1.set_ylabel('발전량(억 KWh)')
+ax2.set_ylabel('전년 대비 증감률(%)')
+
+plt.title('북한 전력 발전량 (1990~2016)', size=30)
+ax1.legend(loc='upper left')
+
+plt.show()
