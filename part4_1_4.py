e670313af37ce852b8e59b9bc03980288f179a6f
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-22 11:10:13 +0900

    Create part4_1_4.py

---
+++ part4_1_4.py
@@ -0,0 +1,17 @@
+
+# example part 4-19
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.style.use('classic')
+
+df = pd.read_csv('./auto-mpg.csv', header=None)
+
+df.columns = ['mpg','cylinders','displacement','horsepower','weight','acceleration','model year','origin','name']
+
+df['mpg'].plot(kind='hist', bins=10, color='coral', figsize=(10,5))
+
+plt.title('Histogram')
+plt.xlabel('mpg')
+plt.show()
