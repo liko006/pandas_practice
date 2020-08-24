a61f4c63cc2768db618f257bfc767c0e56d2c031
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-24 12:55:00 +0900

    Update part4_1_5.py

--- part4_1_5.py
+++ part4_1_5.py
@@ -30,3 +30,23 @@ cylinders_size = df.cylinders/df.cylinders.max() * 300
 df.plot(kind='scatter', x='weight', y='mpg', c='coral', figsize=(10,5), s=cylinders_size, alpha=0.3)
 plt.title('Scatter Plot : mpg-weight-cylinders')
 plt.show()
+
+# example part 4-22
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.style.use('default')
+
+df = pd.read_csv('./auto-mpg.csv', header=None)
+
+df.columns = ['mpg','cylinders','displacement','horsepower','weight','acceleration','model year','origin','name']
+
+cylinders_size = df.cylinders/df.cylinders.max() * 300
+
+df.plot(kind='scatter', x='weight', y='mpg', marker='+', figsize=(10,5), cmap='viridis', c=cylinders_size, s=50, alpha=0.3)
+
+plt.title('Scatter Plot: mpg-weight-cylinders')
+plt.savefig('./scatter.png')
+plt.savefig('./scatter_transparent.png', transparent=True)
+plt.show()
