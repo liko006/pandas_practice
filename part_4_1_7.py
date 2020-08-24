677ae08ef7216cc03043efa403b70987401b0c75
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-24 13:18:18 +0900

    Create part_4_1_7.py

---
+++ part_4_1_7.py
@@ -0,0 +1,33 @@
+# example part 4-24
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+font_path = './malgun.ttf'
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font_name)
+
+plt.style.use('seaborn-poster')
+plt.rcParams['axes.unicode_minus']=False
+
+df = pd.read_csv('./auto-mpg.csv', header=None)
+
+df.columns = ['mpg','cylinders','displacement','horsepower','weight','acceleration','model year','origin','name']
+
+fig = plt.figure(figsize=(15,5))
+ax1 = fig.add_subplot(1,2,1)
+ax2 = fig.add_subplot(1,2,2)
+
+ax1.boxplot(x=[df[df['origin']==1]['mpg'],
+               df[df['origin']==2]['mpg'],
+               df[df['origin']==3]['mpg']], labels=['USA','EU','JP'])
+               
+ax2.boxplot(x=[df[df['origin']==1]['mpg'],
+               df[df['origin']==2]['mpg'],
+               df[df['origin']==3]['mpg']], labels=['USA','EU','JP'], vert=False)
+               
+ax1.set_title('제조국가별 연비 분포(수직 박스 플롯)')
+ax2.set_title('제조국가별 연비 분포(수평 박스 플록)')
+
+plt.show()
+                              
