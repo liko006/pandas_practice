fd1a9354d8b55ade6ce341bae4895ac333064ec8
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-25 15:30:37 +0900

    Update part4_2_1.py

--- part4_2_1.py
+++ part4_2_1.py
@@ -65,3 +65,24 @@ table = titanic.pivot_table(index=['sex'], columns=['class'], aggfunc='size')
 sns.heatmap(table, annot=True, fmt='d', cmap='YlGnBu', linewidth=0.5, cbar=False)
 
 plt.show()
+
+# example part 4-29
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+
+sns.set_style('whitegrid')
+
+fig = plt.figure(figsize=(15,5))
+ax1 = fig.add_subplot(1,2,1)
+ax2 = fig.add_subplot(1,2,2)
+
+sns.stripplot(x='class', y='age', data=titanic, ax=ax1)
+sns.swarmplot(x='class', y='age', data=titanic, ax=ax2)
+
+ax1.set_title('Strip Plot')
+ax2.set_title('Swarm Plot')
+
+plt.show()
