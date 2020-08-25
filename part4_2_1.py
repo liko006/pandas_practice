fd74c0f041a9a4468bfa2d3cf5c46892cca1cccc
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-25 15:25:41 +0900

    Update part4_2_1.py

--- part4_2_1.py
+++ part4_2_1.py
@@ -26,3 +26,42 @@ sns.regplot(x='age', y='fare', data=titanic, ax=ax1)
 sns.regplot(x='age', y='fare', data=titanic, ax=ax2, fit_reg=False)
 
 plt.show()
+
+# example part 4-27
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+
+sns.set_style('darkgrid')
+
+fig = plt.figure(figsize=(15,5))
+ax1 = fig.add_subplot(1,3,1)
+ax2 = fig.add_subplot(1,3,2)
+ax3 = fig.add_subplot(1,3,3)
+
+sns.distplot(titanic['fare'], ax=ax1)
+sns.distplot(titanic['fare'], hist=False, ax=ax2)
+sns.distplot(titanic['fare'], kde=False, ax=ax3)
+
+ax1.set_title('titanic fare - hist/kde')
+ax2.set_title('titanic fare - kde')
+ax3.set_title('titanic fare - hist')
+
+plt.show()
+
+# example part 4-28
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+
+sns.set_style('darkgrid')
+
+table = titanic.pivot_table(index=['sex'], columns=['class'], aggfunc='size')
+
+sns.heatmap(table, annot=True, fmt='d', cmap='YlGnBu', linewidth=0.5, cbar=False)
+
+plt.show()
