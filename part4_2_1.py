742d46ca5c91bb95d1c876ec97e92d2de415bc3c
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-26 18:36:29 +0900

    Update part4_2_1.py

--- part4_2_1.py
+++ part4_2_1.py
@@ -190,3 +190,16 @@ sns.set_style('whitegrid')
 g = sns.FacetGrid(data=titanic, col='who', row='survived')
 
 g = g.map(plt.hist, 'age')
+
+# example part 4-35
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+
+sns.set_style('whitegrid')
+
+titanic_pair = titanic[['age','pclass','fare']]
+
+g = sns.pairplot(titanic_pair)
