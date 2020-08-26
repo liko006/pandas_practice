fbf0105a7397092a203d700df6d60bf2888f2347
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-26 17:58:30 +0900

    Update part4_2_1.py

--- part4_2_1.py
+++ part4_2_1.py
@@ -111,3 +111,49 @@ ax3.set_title('titanic survived - sex/class(stacked)')
 
 plt.show()
 
+# example part 4-31
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+
+sns.set_style('whitegrid')
+
+fig = plt.figure(figsize=(15,5))
+ax1 = fig.add_subplot(1,3,1)
+ax2 = fig.add_subplot(1,3,2)
+ax3 = fig.add_subplot(1,3,3)
+
+sns.countplot(x='class', palette='Set1', data=titanic, ax=ax1)
+sns.countplot(x='class', hue='who', palette='Set2', data=titanic, ax=ax2)
+sns.countplot(x='class', hue='who', palette='Set3', dodge=False, data=titanic, ax=ax3)
+
+ax1.set_title('titanic class')
+ax2.set_title('titanic class - who')
+ax3.set_title('titanic class - who(stacked)')
+
+plt.show()
+
+# example part 4-32
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+
+sns.set_style('whitegrid')
+
+fig = plt.figure(figsize=(15,10))
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,2,3)
+ax4 = fig.add_subplot(2,2,4)
+
+sns.boxplot(x='alive', y='age', data=titanic, ax=ax1)
+sns.boxplot(x='alive', y='age', hue='sex', data=titanic, ax=ax2)
+sns.violinplot(x='alive', y='age', data=titanic, ax=ax3)
+sns.violinplot(x='alive', y='age', hue='sex', data=titanic, ax=ax4)
+
+plt.show()
+
