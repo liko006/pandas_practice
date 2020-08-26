8f088f5b4f4bfd87dde20b8b58721af38e81e59b
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-26 18:33:23 +0900

    Update part4_2_1.py

--- part4_2_1.py
+++ part4_2_1.py
@@ -177,3 +177,16 @@ j3.fig.suptitle('titanic fare - hex', size=15)
 j4.fig.suptitle('titanic fare - kde', size=15)
 
 plt.show()
+
+# example part 4-34
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+
+sns.set_style('whitegrid')
+
+g = sns.FacetGrid(data=titanic, col='who', row='survived')
+
+g = g.map(plt.hist, 'age')
