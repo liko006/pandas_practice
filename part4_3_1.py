c0d20e7bf07de05b3fab61f1539d9a64bd4b7af1
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-27 17:20:28 +0900

    Update part4_3_1.py

--- part4_3_1.py
+++ part4_3_1.py
@@ -20,3 +20,16 @@ seoul_map3 = folium.Map(location=[37.55,126.98], tiles='Stamen Toner', zoom_star
 seoul_map2.save('./seoul2.html')
 seoul_map3.save('./seoul3.html')
 
+# example part 4-38
+
+import pandas as pd
+import folium
+
+df = pd.read_excel('./서울지역 대학교 위치.xlsx', index-col=0)
+
+seoul_map = folium.Map(location=[37.55,126.98], tiles='Stamen Terrain', zoom_start=12)
+
+for name, lat, lng in zip(df.index, df.위도, df.경도):
+    folium.Marker([lat,lng], popup=name).add_to(seoul_map)
+
+seoul_map.save('./seoul_colleges.html')
