4be9f03328c4f7408ca583457580cd69db91a59e
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-29 14:05:41 +0900

    Update part5_1_1.py

--- part5_1_1.py
+++ part5_1_1.py
@@ -1,5 +1,5 @@
 
-# example part 5-1
+# example part 5-1 (Checking NaN data)
 
 import seaborn as sns
 
@@ -16,3 +16,27 @@ print(df.head().isnull())
 print(df.head().notnull())
 
 print(df.head().isnull().sum(axis=0))
+
+# example part 5-2 (Deleting NaN data)
+
+import seaborn as sns
+
+df = sns.load_dataset('titanic')
+
+missing_df = df.isnull()
+for col in missing_df.columns:
+    missing_count = missing_df[col].value_counts()
+    
+    try:
+        print(col, ': ', missing_count[True])
+    except:
+        print(col, ': ', 0)
+
+# droping 'deck' column
+df_thresh = df.dropna(axis=1, thresh=500)
+print(df_thresh.columns)
+
+# deleting rows which have NaN in 'age' column
+df_age = df.dropna(subset=['age'], how='any', axis=0)
+print(len(df_age))
+
