75b9b6da374e4a0efda6ee3e7eefa9c0d7b38bb2
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-30 10:25:42 +0900

    Update part5_2_1.py

--- part5_2_1.py
+++ part5_2_1.py
@@ -17,3 +17,22 @@ print()
 col_dup = df['c2'].duplicated()
 print(col_dup)
 print()
+
+# example part 5-7
+
+import pandas as pd
+
+df = pd.DataFrame({'c1':['a','a','b','a','b'], 'c2':[1,1,1,2,2], 'c3':[1,1,2,2,2]})
+
+print(df)
+print()
+
+# drop the rows if the entire row is duplicated
+df2 = df.drop_duplicates()
+print(df2)
+print()
+
+# drop the rows if the data in given columns are duplicated
+df3 = df.drop_duplicates(subset=['c2','c3'])
+print(df3)
+print()
