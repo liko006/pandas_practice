467a3835d259ec34ba061cd0f1936504491ad9bd
liko006 <67811331+liko006@users.noreply.github.com>
2020-09-01 19:11:37 +0900

    Update part5_6_1.py

--- part5_6_1.py
+++ part5_6_1.py
@@ -26,3 +26,20 @@ print(df.head())
 print()
 print(df.info())
 print()
+
+# example part 5-16
+
+import pandas as pd
+
+dates = ['2019-01-01', '2020-03-01', '2021-06-01']
+
+ts_date = pd.to_datetime(dates)
+print(ts_date)
+print()
+
+pr_day = ts_date.to_period(freq='D')
+print(pr_day)
+pr_month = ts_date.to_period(freq='M')
+print(pr_month)
+pr_year = ts_date.to_period(freq='A')
+print(pr_year)
