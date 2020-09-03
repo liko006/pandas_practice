1ab40a8c827be1f332ae81d541a0bcf9aa37afa8
liko006 <67811331+liko006@users.noreply.github.com>
2020-09-03 18:59:52 +0900

    Create part6_1_2.py

---
+++ part6_1_2.py
@@ -0,0 +1,38 @@
+
+# example part 6-3
+
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+df = titanic.loc[: ,['age','fare']]
+print(df.head())
+print()
+
+def missing_value(series):
+    return series.isnull()
+
+result = df.apply(missing_value, axis=0)
+print(result.head())
+print()
+print(type(result))
+print()
+
+# example part 6-4
+
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+df = titanic.loc[: ,['age','fare']]
+print(df.head())
+print()
+
+def min_max(x):
+    return x.max() - x.min()
+
+result = df.apply(min_max)
+print(result)
+print()
+print(type(result))
+print()
+
+# example part 6-5
