b4e3e45901e78331bb1c2a54fcfe091e69eba979
liko006 <67811331+liko006@users.noreply.github.com>
2020-09-03 19:06:27 +0900

    Update part6_1_2.py

--- part6_1_2.py
+++ part6_1_2.py
@@ -36,3 +36,17 @@ print(type(result))
 print()
 
 # example part 6-5
+
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+df = titanic.loc[: ,['age','fare']]
+df['ten'] = 10
+print(df.head())
+print()
+
+def add_two_obj(a,b):
+    return a + b
+
+df['add'] = df.apply(lambda x: add_two_obj(x['age'], x['ten']), axis=1)
+print(df.head())
