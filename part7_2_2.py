d97f85abc4b84d1638f9660327e45f854d274a6c
liko006 <67811331+liko006@users.noreply.github.com>
2020-09-15 19:14:34 +0900

    Update part7_2_2.py

--- part7_2_2.py
+++ part7_2_2.py
@@ -3,7 +3,7 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 # step 1 ~ 4 데이터 준비
@@ -11,7 +11,7 @@ df = pd.read_csv('./auto-mpg.csv', header=None)
 
 df.columns = ['mpg','cylinders','displacement','horsepower','weight','acceleration','model year','origin','name']
 
-df['horsepwer'].replace('?', np.nan, inplace=True)
+df['horsepower'].replace('?', np.nan, inplace=True)
 df.dropna(subset=['horsepower'], axis=0, inplace=True)
 df['horsepower'] = df['horsepower'].astype('float')
 
@@ -25,3 +25,44 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 print('훈련 데이터: ', X_train.shape)
 print('검증 데이터: ', X_test.shape)
+
+# step 5 비선형회귀분석 모형 - sklearn 사용
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures  # 다항식 변환
+
+poly = PolynomialFeatures(degree=2)
+X_train_poly = poly.fit_transform(X_train)
+
+print('원 데이터: ', X_train.shape)
+print('2차항 변환 데이터: ', X_train_poly.shape)
+
+# 모형 학습
+pr = LinearRegression()
+pr.fit(X_train_poly, y_train)
+
+X_test_poly = poly.fit_transform(X_test)
+r_sq = pr.score(X_test_poly, y_test)
+print(r_sq)
+
+# train데이터의 산점도와 test데이터로 예측한 회귀선 출력
+y_hat_test = pr.predict(X_test_poly)
+
+fig = plt.figure(figsize=(10,5))
+ax = fig.add_subplot(1,1,1)
+ax.plot(X_train, y_train, 'o', label='Train Data')
+ax.plot(X_test, y_hat_test, 'r+', label='Predicted Value')
+ax.legend(loc='best')
+plt.xlabel('weight')
+plt.ylabel('mpg')
+plt.show()
+plt.close()
+
+# 모형 전체 X 데이터로 예측한 y_hat과 실제 값 y의 비교
+X_poly = poly.fit_transform(X)
+y_hat = pr.predict(X_poly)
+
+plt.figure(figsize=(10,5))
+ax1 = sns.distplot(y, hist=False, label='y')
+ax2 = sns.distplot(y_hat, hist=False, label='y_hat', ax=ax1)
+plt.show()
+plt.close()
