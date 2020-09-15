2c1bd0822509eb310316f90c567190715fa306ad
liko006 <67811331+liko006@users.noreply.github.com>
2020-09-15 19:03:14 +0900

    Update part7_2_1.py

--- part7_2_1.py
+++ part7_2_1.py
@@ -64,7 +64,7 @@ plt.close()
 
 # step 4 데이터셋 구분 - train/test
 X=ndf[['weight']]   # 독립변수
-y=ndf[['mpg']]      # 종속변수
+y=ndf['mpg']      # 종속변수
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
