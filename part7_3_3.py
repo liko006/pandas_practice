58645c9424bfd8239e331eb3ebbb2ac2a2e88407
liko006 <67811331+liko006@users.noreply.github.com>
2020-09-21 18:22:43 +0900

    Create part7_3_3.py

---
+++ part7_3_3.py
@@ -0,0 +1,23 @@
+
+# example part 7-6
+
+import pandas as pd
+import numpy as np
+
+# step 1 데이터 준비/기본 설정
+
+uci_path = 'https://archive.ics.uci.edu/ml/machine-learning-databases//breast-cancer-wisconsin/breast-cancer-wisconsin.data'
+df = pd.read_csv(uci_path, header=None)
+
+df.columns = ['id','clump','cell_size','cell_shape','adhesion','epithlial','bare_nuclei','chromatin','normal_nucleoi','mitoses','class']
+
+pd.set_option('display.max_columns', 15)
+
+# step 2 데이터 탐색
+
+print(df.head())
+print()
+print(df.info())
+print()
+print(df.describe())
+print()
