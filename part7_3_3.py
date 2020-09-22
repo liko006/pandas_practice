7e9ddc28978e12f02e41facd47e15e924e243c8c
liko006 <67811331+liko006@users.noreply.github.com>
2020-09-22 13:28:08 +0900

    Update part7_3_3.py

--- part7_3_3.py
+++ part7_3_3.py
@@ -9,7 +9,7 @@ import numpy as np
 uci_path = 'https://archive.ics.uci.edu/ml/machine-learning-databases//breast-cancer-wisconsin/breast-cancer-wisconsin.data'
 df = pd.read_csv(uci_path, header=None)
 
-df.columns = ['id','clump','cell_size','cell_shape','adhesion','epithlial','bare_nuclei','chromatin','normal_nucleoi','mitoses','class']
+df.columns = ['id','clump','cell_size','cell_shape','adhesion','epithlial','bare_nuclei','chromatin','normal_nucleoli','mitoses','class']
 
 pd.set_option('display.max_columns', 15)
 
@@ -29,4 +29,19 @@ df['bare_nuclei'].replace('?', np.nan, inplace=True)
 df.dropna(subset=['bare_nuclei'], axis=0, inplace=True)
 df['bare_nuclei'] = df['bare_nuclei'].astype('int')
 print(df.describe())
+print()
+
+# step 3 데이터 셋 구분
+
+X = df[['clump','cell_size','cell_shape','adhesion','epithlial','bare_nuclei','chromatin','normal_nucleoli','mitoses']]
+y = df['class']
+
+from sklearn import preprocessing
+X = preprocessing.StandardScaler().fit(X).transform(X)
+
+from sklearn.model_selection import train_test_spilt
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=10)
+
+print('train data 개수: ', X_train.shape)
+print('test data 개수: ', X_test.shape)
 print()
