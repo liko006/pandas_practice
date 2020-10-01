446542e63765f86a66759074b28a1294cf2359fc
liko006 <67811331+liko006@users.noreply.github.com>
2020-10-01 13:25:33 +0900

    Update part7_4_1.py

--- part7_4_1.py
+++ part7_4_1.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 # step 1 데이터 준비
 
 # Wholesale customers 데이터 셋 (UCI ML repository)
-uci_path = 'https://archive.ics.uci.edu/ml/machine-learning-databases/\00292/Wholesale%20customers%20data.csv'
+uci_path = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00292/Wholesale%20customers%20data.csv'
 df = pd.read_csv(uci_path, header=0)
 
 # step 2 데이터 탐색
@@ -19,4 +19,16 @@ print()
 print(df.info())
 print()
 print(df.describe())
+print()
+
+# step 3 데이터 전처리
+
+X = df.iloc[:,:]
+print(X[:5])
+print()
+
+from sklearn import preprocessing
+X = preprocessing.StandardScaler.fit(X).transfrom(X)
+
+print(X[:5])
 print()
