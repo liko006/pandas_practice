a33f9e37504696161e78c2116a4f6433a2943e78
liko006 <67811331+liko006@users.noreply.github.com>
2020-10-02 11:10:00 +0900

    Update part7_4_2.py

--- part7_4_2.py
+++ part7_4_2.py
@@ -9,7 +9,7 @@ import folium
 # step 1 데이터 준비/기본 설정
 
 file_path = './2016_middle_school_graduates_report.xlsx'
-df = pd.read_excel(file_path, header=0)
+df = pd.read_excel(file_path, header=0, index_col='Unnamed: 0')
 
 pd.set_option('display.width', None)
 pd.set_option('display.max_rows', 100)
@@ -18,3 +18,12 @@ pd.set_option('display.max_colwidth', 20)
 pd.set_option('display.unicode.east_asian_width', True)
 
 print(df.columns.values)
+
+# step 2 데이터 탐색
+
+print(df.head())
+print()
+print(df.info())
+print()
+print(df.describe())
+print()
